58d4e413749fe70e54724dca9e1635813a3fcc9e
Yudistira Virgus <yudistira.virgus@gmail.com>
2015-04-04 16:16:00 -0400

    Solve problem 3.

---
+++ Algorithms/python/longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,30 @@
+# Source : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+# Author : Yudistira Virgus
+# Date   : 2015-04-04
+#
+# *****************************************************************************************************************************
+# Title : Longest Substring Without Repeating Characters 
+# Problem statement :
+#
+# Given a string, find the length of the longest substring without repeating characters. 
+# For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. 
+# For "bbbbb" the longest substring is "b", with the length of 1.
+#
+# *****************************************************************************************************************************
+#
+# Solution :
+#
+# The idea is using hash table to find the index of the last repeated character  
+class Solution:
+    # @return an integer
+    def lengthOfLongestSubstring(self, s):
+        dic = {}
+        ans, start = 0,-1
+        
+        for i,achar in enumerate(s):
+            if achar in dic:
+                start = max(start, dic[achar])
+            dic[achar] = i
+            ans = max(ans, i - start)
+            
+        return ans
--- README.md
+++ README.md
@@ -5,5 +5,6 @@ LeetCode
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
+|2|[Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)| [Python](./Algorithms/python/longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.py)|Medium|
 |2|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)| [Python](./Algorithms/python/addTwoNumbers/addTwoNumbers.py)|Medium|
 |1|[Two Sum](https://leetcode.com/problems/two-sum/)| [Python](./Algorithms/python/twoSum/twoSum.py)|Medium|
